3696e43c79be82b37864d744be6831b433ef935c
KarzhanevichMaksim <itinmaks@mail.ru>
2023-11-17 10:16:21 +0100

    inkrement dekrement

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,8 +1,6 @@
 // code
-let number1 = Math.floor(Math.random() * 100);
-
-if (number1 % 2 === 0) {
-  console.log(number1 + " : Это четное число");
-} else {
-  console.log(number1 + " : Это нечетное число");
-}
+let number =1;
+alert(number++); 
+alert(++number); 
+alert(number--); 
+alert(--number); 
