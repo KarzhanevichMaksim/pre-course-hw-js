0616f44d546bc65f32e124b5eabdca63567674ed
KarzhanevichMaksim <itinmaks@mail.ru>
2023-11-21 18:11:23 +0100

    test2

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,5 +1,9 @@
 let number = 1;
-console.log(number++);
-console.log(++number);
-console.log(number--);
-console.log(--number);
+let number1=number++;
+let number2=++number;
+let number3=number--;
+let number4=--number;
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
