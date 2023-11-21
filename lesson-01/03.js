b54e24dcd72ce855804da5b76634d39eeeb7509c
KarzhanevichMaksim <itinmaks@mail.ru>
2023-11-21 18:37:42 +0100

    zadanie 3

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,9 +1,5 @@
 let number = 1;
-let number1=number++;
-let number2=++number;
-let number3=number--;
-let number4=--number;
-console.log(number1);
-console.log(number2);
-console.log(number3);
-console.log(number4);
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
